659729bdf75389b7489748ad1bf8d9eaa89f3012
Carlos Mertens <mertenscarlos@yahoo.com>
2023-02-21 23:15:49 +0100

    learn variables

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 let js = 'amazing';
 // if (js === 'amazing') alert('Javascript is FUN!');
 console.log(40 + 8 + 23 - 10);
+
+// Declaring a variable firstName
+// 'Carlos' is a value
+let firstName = 'Carlos';
+
+// underscore and dollar sign are only allowed characters in variables
+let carlos_nicole = 'CN';
+let $function = 27;
+
+// All capital letter for unchanged variables
+let PI = 3.1416;
