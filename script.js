b3b0ad4fd17de604f9ce618ad456fff78933fa2b
Carlos Mertens <mertenscarlos@yahoo.com>
2023-02-23 13:10:46 +0100

    learn string concatenation and template literal

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ let firstName = 'Carlos'; // 'Carlos' is a value
 let carlos_nicole = 'CN';
 let $function = 27;
 
+const lastName = 'Mertens';
 const birthYear = 1980;
 const PI = 3.1416;
 
@@ -24,12 +25,12 @@ job = 'programmer';
 // MATHEMATIC
 // arithmetic operatos (+, -, * and /) - to the power of: **
 let currentYear = 2023;
-console.log(currentYear - birthYear);
+// console.log(currentYear - birthYear);
 // console.log(40 + 8 + 23 - 10);
 // console.log(2 ** 3)
 
 // plus sign can use to concatenate String
-console.log(firstName + ' ' + js);
+// console.log(firstName + ' ' + js);
 
 // COMPARISON
 // use to create boolean values with (>, <, >=, <=)
@@ -57,29 +58,45 @@ x++; // x = x + 1
 // In JS the value has a type and not the variable as in other programming languages
 //
 
-// Numbers: "floating point numbers" -> use for decimals and integers
+// NUMBERS: "floating point numbers" -> use for decimals and integers
 let age = 23;
 let height = 1.65;
 
-// String: use for text
+// STRING: use for text
 let message = 'Hello world!';
 // console.log(typeof message);
 
-// Boolean: logical type that can be true or false
-let ageOverLimit;
+// String concatenation:
+const carlos =
+  "I'm " + firstName + ', a ' + (currentYear - birthYear) + ' years old ' + job;
+console.log(carlos);
+
+// Template literals (template strings):
+const mertens = `I'm ${lastName}, a ${
+  currentYear - birthYear
+} years old ${job}`;
+console.log({ mertens });
+
+console.log(`String with \nnew line \nusing especial character`);
+console.log(`String with
+new line
+using template literal`);
+
+// BOOLEAN: logical type that can be true or false
+let ageOverLimit = true;
 // console.log(typeof ageOverLimit);
 
-// Undefined: use for variavle value that is not yet defined
+// UNDEFINED: use for variavle value that is not yet defined
 let children;
 // console.log({ children });
 
-// Null: use for empty value
+// NULL: use for empty value
 let uniFinished = null;
 // console.log({ uniFinished });
 
-// Symbol: unique value and can not be change
+// SYMBOL: unique value and can not be change
 
-// BigInt: use for larger integer then Number can hold
+// BIGINT: use for larger integer then Number can hold
 
 // Dynamic typing (change the value of a variable)
 ageOverLimit = 21;
