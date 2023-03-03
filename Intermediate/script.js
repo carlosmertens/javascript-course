4cf27808c762442aa838d2f0af6eff8419856a09
Carlos Mertens <mertenscarlos@yahoo.com>
2023-03-03 13:29:36 +0100

    learn function declaration

--- Intermediate/script.js
+++ Intermediate/script.js
@@ -1,1 +1,22 @@
+'use strict';
+/* ********** STRICT MODE ********** */
+// Help us avoid bugs in the code
+// Help us avoid rerserve variable names
+
 /* ********** FUNCTIONS ********** */
+
+// FUNCTION DECLARATION
+function logger() {
+  // Function body. Write your code here...
+  console.log(`My name is Carlos!`);
+}
+// Invoking, calling or running the function
+logger();
+
+// Function with parameters
+function mixJuicer(fruitOne, fruitTwo, fruitThree) {
+  const juice = `Make mix juice with ${fruitOne}, ${fruitTwo}, ${fruitThree}. Thanks!`;
+  return juice;
+}
+// Invoke function with arguments
+console.log(mixJuicer('apples', 'carrots', 'oranges'));
