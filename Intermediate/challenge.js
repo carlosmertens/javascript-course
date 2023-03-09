abcee4d360611efad82b5cff9a76b1a8620b2ce6
Carlos Mertens <mertenscarlos@yahoo.com>
2023-03-09 15:50:25 +0100

    challenge #7

--- Intermediate/challenge.js
+++ Intermediate/challenge.js
@@ -137,3 +137,71 @@ if (person1.calcBMI() > person2.calcBMI()) {
     `${person2.fullName}'s BMI (${person2.bmi}) is higher than ${person1.fullName}'s (${person1.bmi})!`
   );
 }
+/* ///////////////////////////////
+ ********** CODING CHALLENGE #4 **********
+
+Let's improve Steven's tip calculator even more, this time using loops!
+
+Your tasks:
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
+
+Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
+tips and totals arrays 😉 
+
+Bonus:
+4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as an argument. This function calculates the average of all numbers in the given array. This is a difficult challenge (we haven't done this before)! Here is how to solve it:
+4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by the end of the loop, you have all values added together
+4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements)
+4.3. Call the function with the 'totals' array
+
+GOOD LUCK 😀
+
+*/
+console.log('***** CODING CHALLENGE 4 *****');
+
+// Task 1 & 2
+const billsArray = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tipsArray = [];
+const totalsArray = [];
+
+// Task 3
+for (let i = 0; i < billsArray.length; i++) {
+  tipsArray.push(calcTip(billsArray[i]));
+  totalsArray.push(tipsArray[i] + billsArray[i]);
+}
+
+console.log(billsArray);
+console.log(tipsArray);
+console.log(totalsArray);
+
+// Bonus
+// Function to calculate the sum of all elements in an numerical array
+// function sumArray(array) {
+//   let sum = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     sum = sum + array[i];
+//   }
+//   console.log({ sum });
+//   return sum;
+// }
+
+// Function to calculate the average of all elements in an numerical array
+function averageNumericalArray(array) {
+  // const sum = sumArray(array);
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  console.log({ sum });
+  const average = sum / array.length;
+  console.log({ average });
+  return average;
+}
+
+const arrayDummy = [1, 2, 3, 4, 5, 6];
+averageNumericalArray(arrayDummy);
+
+averageNumericalArray(totalsArray);
