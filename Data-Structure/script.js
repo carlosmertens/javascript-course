63104b86190d55a9e3b78c7dec66cc11eb0f7138
Carlos Mertens <mertenscarlos@yahoo.com>
2023-03-23 13:00:57 +0100

    learn spread operator

--- Data-Structure/script.js
+++ Data-Structure/script.js
@@ -35,53 +35,12 @@ const restaurant = {
       `Order for delivery: ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} to be delivery to ${address} at ${time}`
     );
   },
+
+  // Method to display the use of spread operator
+  makePasta: function (ing1, ing2, ing3) {
+    console.log(`Pasta ordered with ${ing1}, ${ing2} and ${ing3}`);
+  },
 };
-
-// Test  Destructure Object With 1 Parameter
-restaurant.orderDelivery({
-  time: '19:00',
-  address: 'Ollenhauser 90',
-  mainIndex: 1,
-  starterIndex: 0,
-});
-
-/* ////////// DESTRUCTURING OBJECTS ///////// */
-console.log('>>> DESTRUCTURING OBJECTS');
-
-// CASE 1:
-const { title, categories, openingHours } = restaurant;
-console.log(title, categories, openingHours);
-
-const {
-  title: restaurantName,
-  openingHours: hours,
-  categories: tags,
-} = restaurant;
-console.log(restaurantName, tags, hours);
-
-// Destructure Default Values
-const {
-  menu = [],
-  salatMenu: course0 = [],
-  mainMenu: course2 = [],
-} = restaurant;
-console.log({ menu }, { course0 }, { course2 });
-
-// Destructure Mutation
-let aa = 999;
-let bb = 777;
-const obj1 = { aa: 21, bb: 22, cc: 23 };
-console.log({ aa }, { bb });
-({ aa, bb } = obj1);
-console.log({ aa }, { bb });
-
-// Destructure Nested Objects
-const {
-  fri: { open, close },
-} = openingHours;
-console.log({ open }, { close });
-
-//
 
 /* ////////// DESTRUCTURING ARRAYS ///////// */
 console.log('>>> DESTRUCTURING ARRAYS');
@@ -133,6 +92,97 @@ const [p, q, r] = arrUnknown;
 console.log(p, q, r); // r will be undefined because it does not exist
 const [defaultP = 1, defaultQ = 0, defaultR = 0] = arrUnknown;
 console.log(defaultP, defaultQ, defaultR);
+
+/* ////////// DESTRUCTURING OBJECTS ///////// */
+console.log('>>> DESTRUCTURING OBJECTS');
+
+// CASE 1:
+const { title, categories, openingHours } = restaurant;
+console.log(title, categories, openingHours);
+
+const {
+  title: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, tags, hours);
+
+// Destructure Default Values
+const {
+  menu = [],
+  salatMenu: course0 = [],
+  mainMenu: course2 = [],
+} = restaurant;
+console.log({ menu }, { course0 }, { course2 });
+
+// Destructure Mutation
+let aa = 999;
+let bb = 777;
+const obj1 = { aa: 21, bb: 22, cc: 23 };
+console.log({ aa }, { bb });
+({ aa, bb } = obj1);
+console.log({ aa }, { bb });
+
+// Destructure Nested Objects
+const {
+  fri: { open, close },
+} = openingHours;
+console.log({ open }, { close });
+
+// Destructure Object With 1 Parameter
+restaurant.orderDelivery({
+  time: '19:00',
+  address: 'Ollenhauser 90',
+  mainIndex: 1,
+  starterIndex: 0,
+});
+
+/* ////////// SPREAD OPERATOR ///////// */
+console.log('>>> SPREAD OPERATOR');
+
+// Spread operator can be use on iterables (arrays, strings, maps, sets)
+// Objects are not iterable but the keys can be spread
+// Spread operators are used only when building and array
+// or pass on a function
+
+// Bad example
+const arr1 = [7, 8, 9];
+const newArr1 = [1, 2, arr1[0], arr1[1], arr1[2]]; // add to more elements
+
+// Good example using spread operator
+const arr2 = [0, 1, 2];
+const newArr2 = [-2, -1, ...arr2];
+console.log(newArr2);
+console.log(...newArr2);
+console.log([...newArr2, 3, 4, 5]);
+
+// Manipulating existing menu in object restaurant
+// Shallow Copies
+const newMainMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMainMenu);
+console.log(restaurant.mainMenu); // note that object was not changed
+
+// Merge
+const mergeArr = [...arr1, ...arr2];
+console.log(mergeArr);
+
+const menuFull = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menuFull);
+
+// Use with string
+const str = 'carlos';
+const letters = [...str];
+console.log(letters);
+
+// Use with function
+// Create a method in restaurant to make pasta with 3 ingredients
+const ingredients = ['fussile', 'red sauce', 'zuccinni'];
+restaurant.makePasta(...ingredients);
+
+// Use with object. Note taht the order no matter
+const newRestaurant = { owner: 'Mertens', ...restaurant, yelpStar: 4 };
+console.log(newRestaurant);
+console.log(restaurant); // note that original object was not changed
 
 // CASE 2:
 const flights =
